fa3761507999a12fdcbf245fc34a09fb879422c4
CIchyFlashy <mateusz.cichos@edu.uekat.pl>
2024-10-28 10:47:12 +0100

    Create zad5.py

---
+++ ćwiczenia2/zad5.py
@@ -0,0 +1,36 @@
+from random import randint
+n = int(input("Podaj rozmiar macierzy: "))
+a = int(input("Podaj początkowy zakres macierzy: "))
+b = int(input("Podaj końcowy zakres macierzy: "))
+
+matrix = [[randint(a,b) for _ in range(n)] for _ in range(n)]
+
+sum_of_first_diagonal = 0
+for i in range(n):
+    sum_of_first_diagonal += matrix[i][i]
+
+for wiersz in matrix:
+    for el_kol in wiersz:
+        print(el_kol, end=" ")
+    print()
+
+print("Suma pierwszej przekątnej wynosi: ", sum_of_first_diagonal)
+
+sum_of_second_diagonal = 0
+for i, y in zip(range(n), range(n-1, -1, -1)):
+    sum_of_second_diagonal += matrix[i][y]
+
+print("Suma drugiej przekątnej: ", sum_of_second_diagonal)
+
+sum_of_odd_positions = 0
+for i in range(n):
+    if i % 2 == 0:
+            continue
+    else:
+        for y in range(n):
+                if y % 2 == 0:
+                    continue
+                else:
+                    sum_of_odd_positions += matrix[i][y]
+
+print("Suma elementów na pozycjach nieparzystych wynosi: ", sum_of_odd_positions)

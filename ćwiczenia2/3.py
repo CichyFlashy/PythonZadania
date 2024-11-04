f7f2811b651265ac966d3b0827b0c90f6eb2753a
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2024-11-04 10:16:03 +0100

    Update 3.py

--- ćwiczenia2/3.py
+++ ćwiczenia2/3.py
@@ -2,4 +2,23 @@ import random
 
 matrix = [[random.randint(-5, 5) for _ in range(5)] for _ in range(5)]
 
+for row in matrix:
+    for el_kol in row:
+        print(el_kol, end=" ")
+    print()
+
+for i in range(5):
+    min = matrix[0][i]
+    max = matrix[0][i]
+    for j in range(5):
+        if matrix[j][i] > max:
+            max = matrix[j][i]
+        if matrix[j][i] < min:
+            min = matrix[j][i]
+    print(f"Maksimum dla kolumny nr {i + 1} wynosi: {max}")
+    print(f"Minimum dla kolumny nr {i + 1} wynosi: {min}")
+    
+
+
+
 

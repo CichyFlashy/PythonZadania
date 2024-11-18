a8eb31acfebb3dcd18d69d3ca4413ef20cd663a2
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2024-11-18 19:43:02 +0100

    Update zad3.py

--- ćwiczenia3/zad3.py
+++ ćwiczenia3/zad3.py
@@ -2,3 +2,13 @@ from random import randint
 list_of_numbers = [randint(0,10) for _ in range(20)]
 print(list_of_numbers)
 
+subsets = [[]]
+for i in list_of_numbers:
+    new_subsets = [subset + [i] for subset in subsets]
+    subsets.extend(new_subsets)
+
+print("Liczba podzbiorów:", len(subsets))
+print("Wszystkie podzbiory:")
+for subset in subsets:
+    print(subset)
+

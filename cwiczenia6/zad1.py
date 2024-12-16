d9af2c682d8994cc961eee6e88b13de3e5fa2231
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2024-12-16 16:56:23 +0100

    Add file zad4.py

--- cwiczenia6/zad1.py
+++ cwiczenia6/zad1.py
@@ -1,9 +1,9 @@
 def szukaj(nazwaPlikWe, nazwaPlikWy, slowo):
     with open(nazwaPlikWe, "r", encoding="utf-8") as plikWe:
         with open(nazwaPlikWy, "w", encoding="utf-8") as plikWy:
-            for line in plikWe:
-                if slowo in line:
-                    plikWy.write(line)
+            for linia in plikWe:
+                if slowo in linia:
+                    plikWy.write(linia)
 
 szukaj("testowy.txt", "wynik.txt", "egzamin")
 
--- cwiczenia6/zad3.py
+++ cwiczenia6/zad3.py
@@ -2,22 +2,22 @@ import os
 
 def sumuj_i_zapisz(nazwaPliku) -> int:
     if not os.path.exists(nazwaPliku):
-        file = open(nazwaPliku, "w")
-        file.write("1\n")
-        file.close()
+        plik = open(nazwaPliku, "w")
+        plik.write("1\n")
+        plik.close()
         return 1
     else:
-        result = 0
-        file = open(nazwaPliku, "r")
-        for line in file:
-            line = line.strip()
-            if line.isdigit():
-                result += int(line)
-        file.close()
-        file = open(nazwaPliku, "a")
-        file.write(f"{result + 1}\n")
-        file.close()
-        return result + 1
+        suma = 0
+        plik = open(nazwaPliku, "r")
+        for linia in plik:
+            linia = linia.strip()
+            if linia.isdigit():
+                suma += int(linia)
+        plik.close()
+        plik = open(nazwaPliku, "a")
+        plik.write(f"{suma + 1}\n")
+        plik.close()
+        return suma + 1
 
 
 print(sumuj_i_zapisz("liczby.txt"))
